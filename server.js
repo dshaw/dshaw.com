237d3c71184a243c31b7991113ccdc7ad57bf043
Daniel D. Shaw <github@dshaw.com>
2012-06-10 02:09:11 -0700

    simplified config.

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 var express = require('express')
-
-var port = process.env.PORT || process.argv[2] || 9000
   , app = express.createServer(express.static(__dirname + '/public'))
 
-app.listen(port)
+app.listen(process.env.PORT || process.argv[2] || 9000)
 
 app.on('listening', function () {
   console.log('listening on :', app.address())
